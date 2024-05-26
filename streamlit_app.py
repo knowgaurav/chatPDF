1584fa6921cf5579bdba5d14a76c44c0d0399a95
Gaurav Singh <sgaurav497@gmail.com>
2024-05-26 17:04:55 +0530

    OpenAI

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
+from langchain_community.callbacks import get_openai_callback
 
 
 def main():
